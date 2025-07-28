bc2f97ef93b35a6f96fcfa1b29cad51aaca10710
Cedar Ren <cedar.ren@gmail.com>
2025-07-27 23:22:10 -0700

    obisidan.nvim

---
+++ nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+-- Set conceallevel for markdown files to properly display Obsidian UI elements
+vim.opt_local.conceallevel = 2
+
+-- Optional: You can also set concealcursor to control when concealing happens
+-- vim.opt_local.concealcursor = 'nc'  -- Conceal in normal and command mode, but not insert mode
---
+++ nvim/lua/custom/plugins/obsidian.lua
@@ -0,0 +1,134 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim", -- Optional but recommended
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Documents/personal",
+      },
+    },
+
+    -- Optional configurations
+    daily_notes = {
+      folder = "daily",
+      date_format = "%Y-%m-%d",
+      template = nil,
+    },
+
+    completion = {
+      nvim_cmp = false,
+      min_chars = 2,
+    },
+
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      }
+    },
+
+    -- Customize how note IDs are generated
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with title
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If no title is given, just use a random suffix
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+
+    -- Optional, set to true if you use the Obsidian Advanced URI plugin
+    use_advanced_uri = false,
+
+    -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground
+    open_app_foreground = false,
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker
+      mappings = {
+        -- Create a new note from your query
+        new = "<C-x>",
+        -- Insert a link to the selected note
+        insert_link = "<C-l>",
+      },
+    },
+
+    -- Optional, configure additional syntax highlighting
+    ui = {
+      enable = true,
+      update_debounce = 200,
+      -- Define checkbox rendering
+      checkboxes = {
+        [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+      },
+      -- Use bullet marks for non-checkbox lists
+      bullets = { char = "•", hl_group = "ObsidianBullet" },
+      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = "ObsidianRefText" },
+      highlight_text = { hl_group = "ObsidianHighlightText" },
+      tags = { hl_group = "ObsidianTag" },
+      block_ids = { hl_group = "ObsidianBlockID" },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`.
+        ObsidianTodo = { bold = true, fg = "#f78c6c" },
+        ObsidianDone = { bold = true, fg = "#89ddff" },
+        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+        ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianBullet = { bold = true, fg = "#89ddff" },
+        ObsidianRefText = { underline = true, fg = "#c792ea" },
+        ObsidianExtLinkIcon = { fg = "#c792ea" },
+        ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianBlockID = { italic = true, fg = "#89ddff" },
+        ObsidianHighlightText = { bg = "#75662e" },
+      },
+    },
+
+    -- Specify how to handle attachments
+    attachments = {
+      -- The default folder to place images in via `:ObsidianPasteImg`.
+      img_folder = "assets/imgs",
+      -- A function that determines the text to insert in the note when pasting an image.
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format("![%s](%s)", path.name, path)
+      end,
+    },
+  },
+}
